8bd668c4595acd075748f06df979984272efa01d
Sayash Chaudhary <sayashchaudhary@gmail.com>
2019-09-27 23:26:21 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
 
 import Layout from './HOC/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -7,16 +8,18 @@ import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
-        </Layout>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://react-burger-app-d2063.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
@@ -89,28 +90,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue !')
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Sayash',
-                address: {
-                    street: 'test street',
-                    zipCode: '3456',
-                    country: 'India'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Sayash',
+        //         address: {
+        //             street: 'test street',
+        //             zipCode: '3456',
+        //             country: 'India'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     };
 
 
